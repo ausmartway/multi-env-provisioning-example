d6a2d7b66bed71fbed345d6e018693978e61ae2f
Yulei Liu <yulei.liu@gmail.com>
2020-01-06 13:06:34 +1100

    fix typo in Name tag

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "aws_instance" "ubuntu" {
   availability_zone = "${lookup(var.configuration, var.environment).region}a"
 
   tags = {
-    name= "${prefix}-multiple-env-demo-${var.environment}"
+    Name= "${prefix}-multiple-env-demo-${var.environment}"
     TTL           = 1
     Owner         = "Yulei"
     instance_type = "${lookup(var.configuration, var.environment).size}"
