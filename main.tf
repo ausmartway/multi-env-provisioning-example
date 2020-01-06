e4d9dfc0eb22f7ed01aef7f0585a04d0fa6b01c3
Yulei Liu <yulei.liu@gmail.com>
2020-01-06 18:02:10 +1100

    change TTL

--- main.tf
+++ main.tf
@@ -22,15 +22,15 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-# resource "aws_instance" "ubuntu" {
-#   ami               = "${data.aws_ami.ubuntu.id}"
-#   instance_type     = "${lookup(var.configuration, var.environment).size}"
-#   availability_zone = "${lookup(var.configuration, var.environment).region}a"
+resource "aws_instance" "ubuntu" {
+  ami               = "${data.aws_ami.ubuntu.id}"
+  instance_type     = "${lookup(var.configuration, var.environment).size}"
+  availability_zone = "${lookup(var.configuration, var.environment).region}a"
 
-#   tags = {
-#     Name= "${var.prefix}-multiple-env-demo"
-#     TTL           = 2
-#     Owner         = "Yulei"
-#     instance_type = "${lookup(var.configuration, var.environment).size}"
-#   }
-# }
+  tags = {
+    Name= "${var.prefix}-multiple-env-demo-${var.enviroment}"
+    TTL           = 1
+    Owner         = "Yulei"
+    instance_type = "${lookup(var.configuration, var.environment).size}"
+  }
+}
